122716344ce06a8dffe159ec66db48c534c2ff39
Bikash Thapa <thapabikash48@gmail.com>
2020-06-09 18:13:27 +0530

    full Screen

--- app/src/main/java/co/smallacademy/myvideoslibrary/Player.java
+++ app/src/main/java/co/smallacademy/myvideoslibrary/Player.java
@@ -2,30 +2,46 @@ package co.smallacademy.myvideoslibrary;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.ConfigurationInfo;
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 public class Player extends AppCompatActivity {
 
     public static final String TAG = "TAG";
     ProgressBar spiiner;
+    ImageView fullScreenOp;
+    FrameLayout frameLayout;
+    VideoView videoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
         spiiner = findViewById(R.id.progressBar);
+        fullScreenOp = findViewById(R.id.fullScreenOp);
+        frameLayout = findViewById(R.id.frameLayout);
 
         Intent i = getIntent();
         Bundle data = i.getExtras();
@@ -35,7 +51,7 @@ public class Player extends AppCompatActivity {
 
         TextView title = findViewById(R.id.videoTitle);
         TextView desc = findViewById(R.id.videoDesc);
-        final VideoView videoPlayer = findViewById(R.id.videoView);
+        videoPlayer = findViewById(R.id.videoView);
 
         title.setText(v.getTitle());
         desc.setText(v.getDescription());
@@ -51,6 +67,18 @@ public class Player extends AppCompatActivity {
                 spiiner.setVisibility(View.GONE);
             }
         });
+        
+        fullScreenOp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                getSupportActionBar().hide();
+                fullScreenOp.setVisibility(View.GONE);
+                frameLayout.setLayoutParams(new ConstraintLayout.LayoutParams(new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)));
+                videoPlayer.setLayoutParams(new FrameLayout.LayoutParams(new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)));
+            }
+        });
 
 
     }
@@ -61,5 +89,21 @@ public class Player extends AppCompatActivity {
             onBackPressed();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        fullScreenOp.setVisibility(View.VISIBLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getSupportActionBar().show();
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        int heightValue = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,220,getResources().getDisplayMetrics());
+        frameLayout.setLayoutParams(new ConstraintLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,heightValue)));
+        videoPlayer.setLayoutParams(new FrameLayout.LayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,heightValue)));
+        int orientation = getResources().getConfiguration().orientation;
+        
+        if(orientation == Configuration.ORIENTATION_PORTRAIT){
+            super.onBackPressed();
+        }
     }
 }
